2f0ae58dbd7780ea9a0155af53619f7d8a957de5
XfHua <jasonHuaxf@163.com>
2020-04-16 14:51:07 +0800

    Add files via upload

---
+++ App209/App209/App209.Android/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Xamarin.Forms;
+
+namespace App209.Droid
+{
+    [Activity(Label = "App209", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        BarcodeModel barcodeModel;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+
+            Handler h = new Handler();
+            Action myAction = () =>
+            {
+                DisplayResult(new Intent());
+            };
+
+            h.PostDelayed(myAction, 5000);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void DisplayResult(Intent intent)
+        {
+            //  Output the scanned barcode to ViewModel
+            barcodeModel.decodedData = intent.GetStringExtra(Resources.GetString(Resource.String.datawedge_intent_key_data));
+            barcodeModel.decodedData = "test";
+
+            MessagingCenter.Send<object, BarcodeModel>(new object(), "barCodeScanned", barcodeModel);
+        }
+    }
+}
---
+++ App209/App209/App209/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App209
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        BarcodeModel barCodeModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            barCodeModel = new BarcodeModel();
+            barCodeModel.decodedData = "defaultValue";
+            this.BindingContext = barCodeModel;
+
+            MessagingCenter.Subscribe<object, BarcodeModel>(new object(), "barCodeScanned", (sender, arg) =>
+            {
+                BarcodeModel tempbarCodeModel = arg as BarcodeModel;
+                barCodeModel.decodedData = tempbarCodeModel.decodedData;
+            });
+        }
+    }
+
+    public class BarcodeModel : INotifyPropertyChanged
+    {
+        private string data;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BarcodeModel()
+        {
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string decodedData
+        {
+            get { return data; }
+            set { data = value; OnPropertyChanged(); }
+        }
+    }
+}
